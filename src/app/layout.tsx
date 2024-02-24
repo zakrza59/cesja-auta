527d10dc33e50224f1279c0f409a8ba2cf701f12
zakrza59 <zakrza59@gmail.com>
2024-02-24 14:25:56 +0100

    add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "~/styles/globals.css";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import { Inter } from "next/font/google";
 
@@ -24,6 +25,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`font-sans ${inter.variable}`}>
         <TRPCReactProvider>{children}</TRPCReactProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
